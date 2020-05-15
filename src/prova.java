f2b9f533734846a59d2fc80dfb23fefcd1a980a1
Beatriz Gomes <bia.beatrizgomes93@gmail.com>
2020-05-15 19:35:46 -0300

    Cabecalho

---
+++ src/prova.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//CABEÇALHO
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("---------------------------------");
+		
+		
+		sc.close();
+	}
+
+
+}
