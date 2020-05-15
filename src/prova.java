655ed0bc8d79d24d5e911515ca5064841cce2e61
Beatriz Gomes <bia.beatrizgomes93@gmail.com>
2020-05-15 19:40:41 -0300

    Leitura volume

--- src/prova.java
+++ src/prova.java
@@ -9,9 +9,19 @@ public class prova {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int qntd;
+		double ticket;
+		
 		//CABEÇALHO
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("---------------------------------");
+		
+		//VOLUME
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.printf("%nQuantas compras o cliente fez no último ano? ");
+		qntd = sc.nextInt();
+		System.out.printf("Qual o ticket médio? ");
+		ticket = sc.nextDouble();
 		
 		
 		sc.close();
